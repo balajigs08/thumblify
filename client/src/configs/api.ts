780a9aef0df9ba41509afb6582a28b8ae519f62e
Balaji GS <balajigs08122004@gmail.com>
2026-05-07 17:24:12 +0530

    fixed api base url

--- client/src/configs/api.ts
+++ client/src/configs/api.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BASE_URL + "/api",
 
   withCredentials: true,
+
 });
 
 export default api;
