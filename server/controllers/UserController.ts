941af804086420a0de759b4df41e4e7faae4967b
Balaji GS <balajigs08122004@gmail.com>
2026-05-07 18:30:45 +0530

    fixed thumbnails fetch

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -2,32 +2,64 @@ import { Request, Response } from 'express';
 import Thumbnail from '../models/Thumbnail.js';
 
 // Controllers to get All User Thumbnails
-export const getUsersThumbnails = async (req: Request, res: Response) => {
+export const getUsersThumbnails = async (
+  req: Request,
+  res: Response
+) => {
+
   try {
+
     const { userId } = req.session;
 
-    const thumbnail = await Thumbnail.find({ userId }).sort({ createdAt: -1 });
+    const thumbnails = await Thumbnail.find({
+      userId
+    }).sort({ createdAt: -1 });
 
-    res.json({ thumbnail });
+    res.json({
+      success: true,
+      thumbnails,
+    });
 
   } catch (error: any) {
+
     console.log(error);
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 
 // Controllers to get single Thumbnail of a User
-export const getThumbnailById = async (req: Request, res: Response) => {
+export const getThumbnailById = async (
+  req: Request,
+  res: Response
+) => {
+
   try {
+
     const { userId } = req.session;
+
     const { id } = req.params;
 
-    const thumbnail = await Thumbnail.findOne({ userId, _id: id });
+    const thumbnail = await Thumbnail.findOne({
+      userId,
+      _id: id,
+    });
 
-    res.json({ thumbnail });
+    res.json({
+      success: true,
+      thumbnail,
+    });
 
   } catch (error: any) {
+
     console.log(error);
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
