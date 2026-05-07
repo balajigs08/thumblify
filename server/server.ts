661fc044aba283bcbecab20b151a89362644c26d
Balaji GS <balajigs08122004@gmail.com>
2026-05-07 14:54:58 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:3000",
+      "http://localhost:3000", "https://thumblify-rosy-tau.vercel.app"
     ],
     credentials: true,
   })
