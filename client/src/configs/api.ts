ee50674aa92489e461a9fdb1d54a0ff51b67513b
Balaji GS <balajigs08122004@gmail.com>
2026-05-08 09:23:43 +0530

    final login fix

--- client/src/configs/api.ts
+++ client/src/configs/api.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: import.meta.env.VITE_BASE_URL + "/api",
+  baseURL:
+    "https://thumblify-server-drab.vercel.app/api",
 
   withCredentials: true,
-
 });
 
 export default api;
